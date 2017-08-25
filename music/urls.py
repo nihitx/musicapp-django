652c4171e8a593ba7cf0b01a4079911c0689b031
nihitx <masnadhossain@live.com>
2017-08-25 21:12:01 +0300

    registration done

--- music/urls.py
+++ music/urls.py
@@ -7,10 +7,17 @@ app_name = 'music'
 
 urlpatterns = [
     #Default homepage (index)
-    url(r'^$', views.index, name='index'),
-    #/music/<album id > /
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    #/music/<album id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    #/music/<album id > /
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    #/music/album/add/
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album_add'),
+    #/music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    #/music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,27 +1,123 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album,Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
-def index (request):
-    all_albums = Album.objects.all()
-    context ={
-        'all_albums' : all_albums
-    }
-    return render(request, 'music/index.html', context)
+###### GENERIC VIEW ######
 
-def detail(request, album_id):
-    album = get_object_or_404(Album,pk=album_id)
-    return render(request, 'music/detail.html',{'album' : album})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def favorite(request, album_id):
-    album = get_object_or_404(Album,pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request,'music/detail.html',{
-        'album': album,
-        'error_message':'you dod not select a valid song',
-        })
-    else:
-        selected_song.is_favorite=True
-        selected_song.save()
-        return render(request, 'music/detail.html',{'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    #model = Album is the same thing as query_set = Album.objects.all and it's the shortcut way of def get_queryset()﻿
+    model = Album
+    template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name , {'form':form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns User objects if credientials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user) #this method logs in the user automatically wtf..
+                    return redirect('music:index')
+
+        return render(request, self.template_name , {'form':form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render, get_object_or_404
+# from .models import Album,Song
+#
+# def index (request):
+#     all_albums = Album.objects.all()
+#     context ={
+#         'all_albums' : all_albums
+#     }
+#     return render(request, 'music/index.html', context)
+#
+# def detail(request, album_id):
+#     album = get_object_or_404(Album,pk=album_id)
+#     return render(request, 'music/detail.html',{'album' : album})
+#
+# def favorite(request, album_id):
+#     album = get_object_or_404(Album,pk=album_id)
+#     try:
+#         selected_song = album.song_set.get(pk=request.POST['song'])
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request,'music/detail.html',{
+#         'album': album,
+#         'error_message':'you dod not select a valid song',
+#         })
+#     else:
+#         selected_song.is_favorite=True
+#         selected_song.save()
+#         return render(request, 'music/detail.html',{'album' : album})
